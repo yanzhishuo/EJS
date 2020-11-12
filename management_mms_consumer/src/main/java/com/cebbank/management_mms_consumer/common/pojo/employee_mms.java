9f836c5ab2c98fd6f74ece124115bbc1b5316150
June-ZCY <45863314+June-ZCY@users.noreply.github.com>
2020-11-12 15:48:51 +0800

    commit management consumer pojo

---
+++ management_mms_consumer/src/main/java/com/cebbank/management_mms_consumer/common/pojo/employee_mms.java
@@ -0,0 +1,12 @@
+package com.cebbank.management_mms_consumer.common.pojo;
+
+/**
+ * @author yanzhishuo
+ * @create 2020-11-12-15:48
+ */
+public class employee_mms {
+    public static void main(String[] args) {
+
+    }
+
+}
