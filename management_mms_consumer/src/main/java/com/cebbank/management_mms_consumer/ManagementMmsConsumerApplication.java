10ecc4c9643a579afa41c78f2bc73edaf69fda60
June-ZCY <45863314+June-ZCY@users.noreply.github.com>
2020-11-12 15:22:34 +0800

    commit management

---
+++ management_mms_consumer/src/main/java/com/cebbank/management_mms_consumer/ManagementMmsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.cebbank.management_mms_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementMmsConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementMmsConsumerApplication.class, args);
+    }
+
+}
---
+++ management_mms_consumer/src/test/java/com/cebbank/management_mms_consumer/ManagementMmsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cebbank.management_mms_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMmsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
